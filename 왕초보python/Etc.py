a0c91116e87f45c3c49d33233d671c9988e28e75
no1-allthings <no1.allthings@gmail.com>
2021-04-30 19:53:23 +0900

    왕초보-1

---
+++ 왕초보python/Etc.py
@@ -0,0 +1,30 @@
+# a=divmod(50,8) # 몫과 나머지같이 계산하는 함수
+# print(a)
+#
+# price = 5000
+# price *=0.85
+# print(price)
+#
+# r = 0.8
+# t = 110
+# print(r*t)
+#
+# family = ['father', 'mother', 'i', ]
+# print(len(family))
+# print(family[2])
+# c = family[2]='i am'
+# print(family)
+# family.insert(0,1)
+# print(family)
+# del family[3]
+# print(family)
+#
+# print('직 삼각형 그리기 \n')
+# leg =int(input('변의길이: '))
+# for i in range(leg):
+#     print('@ '* (i+1))
+# erea = i**2 / 2
+# print(erea)
+
+h= int(input('입력: '))
+print(h**2)
---
+++ 왕초보python/제어문.py
@@ -0,0 +1,29 @@
+# for i in range(1,11):
+#     print(i, end="\n")
+
+# num = int(input('입력: '))
+# c=1
+# while c<=num:
+#     print(c, c**2)
+#     c +=1
+
+
+# n = int(input('숫자입력: '))
+# if n == 1:
+#     print('일')
+# elif n == 2:
+#     print('이')
+# else:
+#     print('삼')
+
+# sum = 0
+# while True:
+#     cc = int(input('입력: '))
+#     if cc<0:
+#         break
+#     else:
+#         sum += cc
+#     print(sum)
+
+year = int(input('연도를 입력하세요: '))
+
