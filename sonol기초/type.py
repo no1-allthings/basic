5ccd25f407955209e5a0dfa6c8944103846c3e16
no1-allthings <no1.allthings@gmail.com>
2021-05-12 19:43:50 +0900

    왕초보6

---
+++ sonol기초/type.py
@@ -0,0 +1,33 @@
+# a=5
+# b='5'
+# print(a*b)
+# print(b*a)
+# print(int(12.9))
+#
+# num=5.5
+# print(range(int(num)))
+#
+# g = input()
+# print(g)
+
+# y, m, d = map(int,input('yy.mm.dd: ').split('.'))
+# print(y,m,d)
+# print(type(y))
+#
+# g,h,j = list(map(int,input('세게 입력: ').split()))
+# print(g,h,j)
+# print(type(g))
+
+x=3
+y=5
+print(x,'과',y,'의 합',x+y,'이다')
+print(str(x)+'과'+str(y)+'의 합'+str(x+y)+'이다')
+print(x,end='')
+print('과',end='')
+print(y,end='')
+print('의 합',end='')
+print(x+y,     end='')
+print('이다')
+
+print(f'{x}과{y}의 합{x+y}이다')
+
